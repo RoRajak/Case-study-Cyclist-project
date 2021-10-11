f611416d1fc84c0bf9de89235735a2ff30492794
Rohit Rajak <45961366+RoRajak@users.noreply.github.com>
2021-10-11 21:09:53 +0530

    Update CapstoneProject.R

--- CapstoneProject.R
+++ CapstoneProject.R
@@ -1,32 +1,32 @@
 # Setting up my environment
 # Importing library `tidyverse` and loading datasets
 library(tidyverse)
-tripdata_202004 <- read_csv("202004-divvy-tripdata.csv")
-tripdata_202005 <- read_csv("202005-divvy-tripdata.csv")
-tripdata_202006 <- read_csv("202006-divvy-tripdata.csv")
-tripdata_202007 <- read_csv("202007-divvy-tripdata.csv")
-tripdata_202008 <- read_csv("202008-divvy-tripdata.csv")
-tripdata_202009 <- read_csv("202009-divvy-tripdata.csv")
-tripdata_202010 <- read_csv("202010-divvy-tripdata.csv")
-tripdata_202011 <- read_csv("202011-divvy-tripdata.csv")
-tripdata_202012 <- read_csv("202012-divvy-tripdata.csv")
-tripdata_202101 <- read_csv("202101-divvy-tripdata.csv")
-tripdata_202102 <- read_csv("202102-divvy-tripdata.csv")
-tripdata_202103 <- read_csv("202103-divvy-tripdata.csv")
+data_202004 <- read_csv("202004-divvy-tripdata.csv")
+data_202005 <- read_csv("202005-divvy-tripdata.csv")
+data_202006 <- read_csv("202006-divvy-tripdata.csv")
+data_202007 <- read_csv("202007-divvy-tripdata.csv")
+data_202008 <- read_csv("202008-divvy-tripdata.csv")
+data_202009 <- read_csv("202009-divvy-tripdata.csv")
+data_202010 <- read_csv("202010-divvy-tripdata.csv")
+data_202011 <- read_csv("202011-divvy-tripdata.csv")
+data_202012 <- read_csv("202012-divvy-tripdata.csv")
+data_202101 <- read_csv("202101-divvy-tripdata.csv")
+data_202102 <- read_csv("202102-divvy-tripdata.csv")
+data_202103 <- read_csv("202103-divvy-tripdata.csv")
 
 # Combining all datasets
-tripdata <- rbind(tripdata_202004,
-                  tripdata_202005,
-                  tripdata_202006,
-                  tripdata_202007,
-                  tripdata_202008,
-                  tripdata_202009,
-                  tripdata_202010,
-                  tripdata_202011,
-                  tripdata_202012,
-                  tripdata_202101,
-                  tripdata_202102,
-                  tripdata_202103)
+tripdata <- rbind(data_202004,
+                  data_202005,
+                  data_202006,
+                  data_202007,
+                  data_202008,
+                  data_202009,
+                  data_202010,
+                  data_202011,
+                  data_202012,
+                  data_202101,
+                  data_202102,
+                  data_202103)
 
 glimpse(tripdata)
 
@@ -142,9 +142,7 @@ alltrips <- tripdata_cleaned %>%
 
 alltrips$day_of_week <- wday(alltrips$started_at, label = TRUE)
 library(xlsx)
-write.xlsx(alltrips, file = "all_trips.xlsx", row.names = FALSE)
-write.csv(alltrips, file = "all_trips.csv", row.names = FALSE)
-
+# divided my largedataset into 1048000 size so i will have 4 csv file.
 my_file_rows <- seq(1, nrow(alltrips))
 chunk_size <- 1048000
 library(data.table)
@@ -156,16 +154,3 @@ lapply(split(my_file_rows, ceiling(my_file_rows/chunk_size)), function(x){
   
 })
 
-step1 = 1000
-runto = nrow(alltrips)
-nsteps = ceiling(runto/step1)
-for( part in seq_len(nsteps) ) { # part = 1
-  cat( part, 'of', nsteps, '\n')
-  fr = (part-1)*step1 + 1
-  to = min(part*step1, runto)
-  
-  my_df1 = alltrips[fr:to,]
-  # ...
-  write.table(str_data, file = paste0("all_trips",fr,"-",to,".csv"))
-}
-rm(part, step1, runto, nsteps, fr, to)
